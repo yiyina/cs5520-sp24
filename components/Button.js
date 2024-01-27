d25b604c1492dd6d0d2c803838a25a040f4a9fcd
YINA YI <yinayi@YINAs-MacBook-Pro.local>
2024-01-27 15:13:48 -0800

    added conmments on Button.js

--- components/Button.js
+++ components/Button.js
@@ -2,15 +2,19 @@ import { StyleSheet, Text, TouchableOpacity } from 'react-native'
 import React from 'react'
 import colors from './Colors';
 
-/* 
- * Function: Button
- * Purpose: render a button
- * Parameters: text, onPress, color
- * Return: a button
+/**
+ * Render the Button component.
+ * 
+ * @param {object} props - The component's props.
+ * @param {string} props.text - The button text.
+ * @param {function} props.onPress - Callback function to handle the button press.
+ * @param {string} [props.color="black"] - The button color (optional).
+ * @returns {JSX.Element} - The Button component.
  */
 export default function Button({ text, onPress, color }) {
     const textColor = {color: colors[color] || colors.black};
 
+    // Render the Button component
     return (
         <TouchableOpacity onPress={onPress}>
             <Text style={[textColor, styles.margin]}>{text}</Text>
